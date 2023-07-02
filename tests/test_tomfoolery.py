89e9e617651e9bc8bc870daef77b41bbf8fbe1f4
matt manes <mattmanes@pm.me>
2023-07-01 20:18:15 -0500

    tests: add and update

--- tests/test_tomfoolery.py
+++ tests/test_tomfoolery.py
@@ -1,16 +1,18 @@
 import pytest
 from pathier import Pathier
-
-from tomfoolery import TomFoolery
+import ast_comments as ast
+from tomfoolery import TomFoolery, generate_from_file
+import sys
 
 root = Pathier(__file__).parent
 
 
 def test__engine():
-    (root / "venue.py").write_text("")
-    engine = TomFoolery()
-    engine.generate_from_file(root / "venue.toml", True)
-    assert (root / "venue.py").exists()
+    datafile = root / "venue.toml"
+    pyfile = datafile.with_suffix(".py")
+    pyfile.delete()
+    generate_from_file(datafile, pyfile)
+    assert pyfile.exists()
 
 
 def test__dataclass():
@@ -22,4 +24,37 @@ def test__dataclass():
     assert venue.name == venuetoml["name"]
     assert venue.address.city == venuetoml["address"]["city"]
     assert type(venue.calendar.num_events) == int
-    (root / "venue.py").write_text("")
+
+
+def test__no_overwrite():
+    datafile = root / "venue.toml"
+    src = (root / "venue2.py").read_text()
+    module = ast.parse(src)
+    engine = TomFoolery(module)  # type:ignore
+    tom = engine.generate(datafile.stem, datafile.loads())
+    (root / "venue22.py").write_text(tom)
+    assert src == tom
+
+
+def test__update():
+    datafile = root / "venue.toml"
+    data = datafile.loads()
+    original = datafile.loads()
+    data["yee"] = "haw"
+    data["afraid_of_the_dark"] = {"me": False, "you": True}
+    datafile.dumps(data)
+    generate_from_file(datafile)
+    try:
+        sys.modules.pop("venue")
+    except Exception as e:
+        pass
+    from venue import Venue
+
+    venue = Venue.load()
+    venuetoml = (root / "venue.toml").loads()
+    assert venue.name == venuetoml["name"]
+    assert venue.address.city == venuetoml["address"]["city"]
+    assert venue.yee == "haw"
+    assert not venue.afraid_of_the_dark.me
+    assert venue.afraid_of_the_dark.you
+    datafile.dumps(original)
---
+++ tests/venue.py
@@ -0,0 +1,46 @@
+from dataclasses import asdict, dataclass
+
+import dacite
+from pathier import Pathier, Pathish
+from typing_extensions import Self
+
+
+@dataclass
+class Address:
+    street: str
+    city: str
+    state: str
+    zip_code: int
+
+
+@dataclass
+class Calendar:
+    num_events: int
+    start_month: str
+    stop_month: str
+
+
+@dataclass
+class AfraidOfTheDark:
+    me: bool
+    you: bool
+
+
+@dataclass
+class Venue:
+    name: str
+    address: Address
+    calendar: Calendar
+    yee: str
+    afraid_of_the_dark: AfraidOfTheDark
+
+    @classmethod
+    def load(cls, path: Pathish = Pathier(__file__).parent / "venue.toml") -> Self:
+        """Return a `datamodel` object populated from `path`."""
+        data = Pathier(path).loads()
+        return dacite.from_dict(cls, data)
+
+    def dump(self, path: Pathish = Pathier(__file__).parent / "venue.toml"):
+        """Write the contents of this `datamodel` object to `path`."""
+        data = asdict(self)
+        Pathier(path).dumps(data)

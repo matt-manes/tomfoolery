c84e877470044a7127a50064f4de2a15afa82c58
matt manes <mattmanes@pm.me>
2024-02-06 12:56:46 -0600

    docs: update load and dump docstrings

--- src/tomfoolery/_dump.py
+++ src/tomfoolery/_dump.py
@@ -1,4 +1,4 @@
 def dump(self, path: Pathish = Pathier(__file__).parent / "filepath"):
-    """Write the contents of this `datamodel` object to `path`."""
+    """Write the contents of this instance to `path`."""
     data = asdict(self)
     Pathier(path).dumps(data)
